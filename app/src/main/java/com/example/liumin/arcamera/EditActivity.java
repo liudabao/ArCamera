ab3fcd65cfc7014f1a5720369b86601bac8430d3
liudabao <15810163587@163.com>
2016-09-25 22:17:21 +0800

    Second

--- app/src/main/java/com/example/liumin/arcamera/EditActivity.java
+++ app/src/main/java/com/example/liumin/arcamera/EditActivity.java
@@ -171,7 +171,13 @@ public class EditActivity extends Activity {
         share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showShare();
+                if(path!=null){
+                    showShare();
+                }
+                else{
+                    Toast.makeText(EditActivity.this, "没有可分享的图片", Toast.LENGTH_LONG).show();
+                }
+               // showShare();
                 //Toast.makeText(EditActivity.this, "test", Toast.LENGTH_LONG).show();
             }
         });
@@ -222,19 +228,20 @@ public class EditActivity extends Activity {
         oks.disableSSOWhenAuthorize();
 
         // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
-        oks.setTitle("标题");
+        oks.setTitle("我的Ar图片");
         // titleUrl是标题的网络链接，QQ和QQ空间等使用
         oks.setTitleUrl("http://sharesdk.cn");
         // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
+        oks.setText("我拍了一张Ar图片，你也来试试");
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        Log.e("Share", "path is "+path);
+        oks.setImagePath(path);//确保SDcard下面存在此张图片
         // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");
+        //oks.setUrl("http://sharesdk.cn");
         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("我是测试评论文本");
+        //oks.setComment("我是测试评论文本");
         // site是分享此内容的网站名称，仅在QQ空间使用
-        oks.setSite(getString(R.string.app_name));
+        oks.setSite("我的Ar图片");
         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
         oks.setSiteUrl("http://sharesdk.cn");
         // 启动分享GUI
